63f26e88ef53378016418f28a3075183e842d2e1
Rahul Panchal <rm5901960@gmail.com>
2024-07-25 11:43:36 +0530

    Add error handling

--- utils/getUsername.js
+++ utils/getUsername.js
@@ -1,9 +1,9 @@
 const jwt=require('jsonwebtoken')
 const getUserData=(req,res)=>{
+    const auth = req.headers.authorization;
+    const token = auth.split(' ')[1];
+    const userData = jwt.decode(token,'cat');
     try{
-        const auth = req.headers.authorization;
-        const token = auth.split(' ')[1];
-        const userData = jwt.decode(token,'cat');
         console.log("🔥 userdata: ",userData)
         res.status(200).json({data: userData});
     }catch(e){
